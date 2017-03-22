3dd0298863cfecdae17c920a362c09e70f062793
Felipe Scrochio Custódio <felipe.crochi@gmail.com>
2017-03-22 11:23:09 -0300

    Mouse function added

--- t1/t1.cpp
+++ t1/t1.cpp
@@ -14,7 +14,7 @@ void draw(void)
 {
 
         // Background color
-        glClearColor(1, 1, 1, 1);
+        glClearColor(0.0f, 0.0f, 0.0f, 1);
         // Paint background
         glClear(GL_COLOR_BUFFER_BIT);
 
@@ -29,13 +29,28 @@ void draw(void)
         glFlush();
 }
 
+void on_mouseClick(int button, int click_state, int x_mouse_position, int y_mouse_position)
+{
+	if (click_state == GLUT_DOWN) {
+		if (button == GLUT_RIGHT_BUTTON) {
+			// Rotate blades clockwise
+		} else if (button == GLUT_LEFT_BUTTON) {
+                        // Rotate blades counter clockwise
+		}
+	}
+
+	glutPostRedisplay(); // Forces scene redraw
+}
+
 int main(int argc, char* argv[])
 {
         glutInit(&argc, argv); // Instanciate Glut
         glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); // Defines the buffer display mode
         glutInitWindowSize(800, 600); // Defines the size in pixels of the window
-        glutCreateWindow("Wind Vane"); // Defines the window title
+        glutCreateWindow("Windmill"); // Defines the window title
         glutDisplayFunc(draw); // Set rendering function as "draw(.)"
+
+        glutMouseFunc(on_mouseClick); // Handles mouse clicks
 
         gluOrtho2D(0, 400, -150, 150); // Defines the orthogonal plane to build the scene in
 
