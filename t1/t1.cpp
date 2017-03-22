46dfcfe7b7c31841c2fc74eebd19e83ebaff2dff
Felipe Scrochio Custódio <felipe.crochi@gmail.com>
2017-03-22 10:44:34 -0300

    Assignment template ready

--- t1/t1.cpp
+++ t1/t1.cpp
@@ -1,11 +1,29 @@
+/**
+ * SCC0250 - Computação Gráfica
+ * Trabalho 1 - Catavento
+ * Alunos:
+ *      Felipe Scrochio Custódio - 9442688
+ *      Henrique Loschiavo -
+ *      Lucas Antognoni -
+ *      Gustavo Santiago -
+ **/
+
 #include <GL/glut.h>
 
 void draw(void)
 {
-        /* Primitives */
-        glBegin(GL_LINES);
-        glVertex2f(0, -150);
-        glVertex2f(400, 150);
+
+        // Background color
+        glClearColor(1, 1, 1, 1);
+        // Paint background
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+            glColor3f(0.0f, 0.88f, 0.95f);
+            glVertex2f(100, -50);
+            glColor3f(0.96f, 0.38f, 0.56f);
+            glVertex2f(200, 100);
+            glVertex2f(300, -50);
         glEnd();
 
         glFlush();
@@ -15,8 +33,8 @@ int main(int argc, char* argv[])
 {
         glutInit(&argc, argv); // Instanciate Glut
         glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); // Defines the buffer display mode
-        glutInitWindowSize(400, 300); // Defines the size in pixels of the window
-        glutCreateWindow("Exemplo OpenGL"); // Defines the window title
+        glutInitWindowSize(800, 600); // Defines the size in pixels of the window
+        glutCreateWindow("Wind Vane"); // Defines the window title
         glutDisplayFunc(draw); // Set rendering function as "draw(.)"
 
         gluOrtho2D(0, 400, -150, 150); // Defines the orthogonal plane to build the scene in
