8342e18d2622951f6dd30ba2b051f9ca63bfe926
Felipe Scrochio Custódio <felipe.crochi@gmail.com>
2017-03-25 18:40:20 -0300

    Added mouse rotation event

--- t1/src/t1.cpp
+++ t1/src/t1.cpp
@@ -3,7 +3,7 @@
  * Trabalho 1 - Windmill
  * Alunos:
  *      Felipe Scrochio Custódio - 9442688
- *      Henrique Loschiavo - 
+ *      Henrique Loschiavo -
  *      Lucas Antognoni -
  *      Gustavo Santiago - 8937416
  **/
@@ -11,48 +11,8 @@
 #include <GL/glut.h>
 #include <math.h>
 
-void changeSize(int w, int h) {
-
-	// Prevent a divide by zero, when window is too short
-	// (you cant make a window of zero width).
-	if (h == 0)
-	{
-		h = 1;
-	}
-
-	float ratio = 1.0* w / h;
-
-	// Use the Projection Matrix
-	glMatrixMode(GL_PROJECTION);
-
-	// Reset Matrix
-	glLoadIdentity();
-
-	// Set the viewport to be the entire window
-	glViewport(0, 0, w, h);
-
-	// Set the correct perspective.
-	gluPerspective(45,ratio,1,600);
-
-	// Get Back to the Modelview
-	glMatrixMode(GL_MODELVIEW);
-}
-
-void rotate_around() {
-
-	GLfloat temp_x, temp_y;
-	// Recalculate 1st vertex
-	x_1 = x_1 - c_x;
-	y_1 = y_1 - c_y;
-	temp_x = x_1;
-	temp_y = y_1;
-	x_1 = temp_x * cos(0.00174533) - temp_y * sin(0.00174533);
-	y_1 = temp_y * cos(0.00174533) + temp_x * sin(0.00174533);
-	x_1 += c_x;
-	y_1 += c_y;
-
-}
-
+GLfloat c_x = 200;
+GLfloat c_y = 0;
 GLfloat x_1 = 100;
 GLfloat y_1 = 0;
 GLfloat x_2 = 150;
@@ -60,11 +20,6 @@ GLfloat y_2 = 50;
 
 void draw(void)
 {
-	GLfloat temp_x, temp_y;
-        GLfloat c_x, c_y;
-        c_x = 200;
-        c_y = 0;
-
         // Background color
         glClearColor(0.0f, 0.0f, 0.0f, 1);
 
@@ -79,56 +34,99 @@ void draw(void)
                 glVertex2f(c_x, c_y);
         glEnd();
 
-	// Blade
+	// Post
+	glBegin(GL_QUADS);
+		glColor3f(1.0, 0.59, 0.26);
+		glVertex2f(c_x - 10, c_y - 100);
+		glVertex2f(c_x - 10, c_y);
+		glColor3f(0.3, 0.17, 0.0);
+		glVertex2f(c_x + 10, c_y);
+		glVertex2f(c_x + 10, c_y - 100);
+	glEnd();
+
+	// Blades
 	glBegin(GL_TRIANGLES);
-            glColor3f(0.0f, 0.88f, 0.95f);
-            glVertex2f(c_x, c_y);
-            glColor3f(0.96f, 0.38f, 0.56f);
-            glVertex2f(x_1, y_1);
-            glVertex2f(x_2, y_2);
-        glEnd();
+	    glColor3f(0.0f, 0.88f, 0.95f);
+	    glVertex2f(c_x, c_y);
+	    glColor3f(0.96f, 0.38f, 0.56f);
+	    glVertex2f(x_1, y_1);
+	    glVertex2f(x_2, y_2);
+	glEnd();
 
-	// Recalculate 1st vertex
-        x_1 = x_1 - c_x;
-        y_1 = y_1 - c_y;
-        temp_x = x_1;
-        temp_y = y_1;
-        x_1 = temp_x * cos(0.00174533) - temp_y * sin(0.00174533);
-        y_1 = temp_y * cos(0.00174533) + temp_x * sin(0.00174533);
-        x_1 += c_x;
-        y_1 += c_y;
+	glBegin(GL_TRIANGLES);
+	    glColor3f(0.0f, 0.88f, 0.95f);
+	    glVertex2f(c_x, c_y);
+	    glColor3f(0.96f, 0.38f, 0.56f);
+	    glVertex2f(x_1 + 20, y_1);
+	    glVertex2f(x_2 + 20, y_2);
+	glEnd();
 
-	// Recalculate 2nd vertex
-	x_2 = x_2 - c_x;
-        y_2 = y_2 - c_y;
-        temp_x = x_2;
-        temp_y = y_2;
-        x_2 = temp_x * cos(0.00174533) - temp_y * sin(0.00174533);
-        y_2 = temp_y * cos(0.00174533) + temp_x * sin(0.00174533);
-        x_2 += c_x;
-        y_2 += c_y;
+	glBegin(GL_TRIANGLES);
+	    glColor3f(0.0f, 0.88f, 0.95f);
+	    glVertex2f(c_x, c_y);
+	    glColor3f(0.96f, 0.38f, 0.56f);
+	    glVertex2f(x_1 + 50, y_1);
+	    glVertex2f(x_2 + 20, y_2);
+	glEnd();
 
-        glBegin(GL_POINTS);
-                glColor3f(0.0f, 0.0f, 1.0f);
-                glVertex2f(x_1, y_1);
-        glEnd();
-
-	glBegin(GL_POINTS);
-                glColor3f(0.0f, 0.0f, 1.0f);
-                glVertex2f(x_2, y_2);
-        glEnd();
+	glBegin(GL_TRIANGLES);
+	    glColor3f(0.0f, 0.88f, 0.95f);
+	    glVertex2f(c_x, c_y);
+	    glColor3f(0.96f, 0.38f, 0.56f);
+	    glVertex2f(x_1 + 100, y_1);
+	    glVertex2f(x_2 + 100, y_2);
+	glEnd();
 
         glFlush();
 }
 
 void on_mouseClick(int button, int click_state, int x_mouse_position, int y_mouse_position)
 {
-
+	GLfloat temp_x, temp_y;
 	if (click_state == GLUT_DOWN) {
 		if (button == GLUT_RIGHT_BUTTON) {
-                        // Rotate clockwise
+
+			// Recalculate 1st vertex
+		        x_1 = x_1 - c_x;
+		        y_1 = y_1 - c_y;
+		        temp_x = x_1;
+		        temp_y = y_1;
+		        x_1 = temp_x * cos(0.5) + temp_y * sin(0.5);
+		        y_1 = temp_y * cos(0.5) - temp_x * sin(0.5);
+		        x_1 += c_x;
+		        y_1 += c_y;
+
+			// Recalculate 2nd vertex
+			x_2 = x_2 - c_x;
+		        y_2 = y_2 - c_y;
+		        temp_x = x_2;
+		        temp_y = y_2;
+		        x_2 = temp_x * cos(0.5) + temp_y * sin(0.5);
+		        y_2 = temp_y * cos(0.5) - temp_x * sin(0.5);
+		        x_2 += c_x;
+		        y_2 += c_y;
+
 		} else if (button == GLUT_LEFT_BUTTON) {
-                        // Rotate counter clockwise
+
+			// Recalculate 1st vertex
+		        x_1 = x_1 - c_x;
+		        y_1 = y_1 - c_y;
+		        temp_x = x_1;
+		        temp_y = y_1;
+		        x_1 = temp_x * cos(0.5) - temp_y * sin(0.5);
+		        y_1 = temp_y * cos(0.5) + temp_x * sin(0.5);
+		        x_1 += c_x;
+		        y_1 += c_y;
+
+			// Recalculate 2nd vertex
+			x_2 = x_2 - c_x;
+		        y_2 = y_2 - c_y;
+		        temp_x = x_2;
+		        temp_y = y_2;
+		        x_2 = temp_x * cos(0.5) - temp_y * sin(0.5);
+		        y_2 = temp_y * cos(0.5) + temp_x * sin(0.5);
+		        x_2 += c_x;
+		        y_2 += c_y;
 		}
 	}
 
@@ -143,8 +141,8 @@ int main(int argc, char* argv[])
         glutCreateWindow("Windmill"); // Defines the window title
 
         glutDisplayFunc(draw); // Set rendering function as "draw()"
-        // glutReshapeFunc(changeSize);
-        glutIdleFunc(draw);
+
+	glutIdleFunc(draw); // Set draw to repeat while no events occur
 
         glutMouseFunc(on_mouseClick); // Handles mouse clicks
 
