4376671aa3c2428b49faebfbd011cb718101302b
HenriqueLosc <henrique.losc@gmail.com>
2017-03-31 11:13:11 -0300

    Completei TODOs, mudei sistema de coordenadas do quadrado

---
+++ t1/CMakeFiles/t1.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/dvco/bcc/cg/cg/t1/src/main.c" "/home/dvco/bcc/cg/cg/t1/CMakeFiles/t1.dir/src/main.c.o"
+  "/home/dvco/bcc/cg/cg/t1/src/shapes.c" "/home/dvco/bcc/cg/cg/t1/CMakeFiles/t1.dir/src/shapes.c.o"
+  "/home/dvco/bcc/cg/cg/t1/src/transforms.c" "/home/dvco/bcc/cg/cg/t1/CMakeFiles/t1.dir/src/transforms.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "includes"
+  "src"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
