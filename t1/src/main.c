146906a886fc9224210e93ced66b9d3d2b7e1728
meist3r-ed <gs4n.96@gmail.com>
2017-03-31 15:06:33 -0300

    Reshaping function added, minor corrections to buffering and display mode

--- t1/src/main.c
+++ t1/src/main.c
@@ -13,6 +13,35 @@
 #include "shapes.h"
 #include "transforms.h"
 
+#define ORTHO_X 300
+#define ORTHO_Y 300
+
+/* Reshaping function */
+void reshape(int width, int height){
+    // Screen can't be smaller than 0
+    if(height == 0)
+        height = 1;
+
+    float ratio = width * 1.0/height;
+
+    // Changes matrix mode to projection
+    glMatrixMode(GL_PROJECTION);
+
+    // Reset Matrix
+	glLoadIdentity();
+
+    // Viewport correction
+    glViewport(0, 0, width, height);
+
+    // Perspective and projection correction
+    //gluPerspective(45, ratio, -1, 1);
+    gluOrtho2D(-ORTHO_X * ratio, ORTHO_X * ratio, -ORTHO_Y, ORTHO_Y);
+
+    // Switches matrix mode back to modelview
+    glMatrixMode(GL_MODELVIEW);
+}
+
+
 /* Draw loop */
 void draw(void)
 {
@@ -22,6 +51,12 @@ void draw(void)
         // Repaint screen
         glClear(GL_COLOR_BUFFER_BIT);
 
+        // Set matrix mode to MODELVIEW
+        glMatrixMode(GL_MODELVIEW);
+
+        // Reset Matrix
+    	glLoadIdentity();
+
         glBegin(GL_POINTS);
                 glColor3f(1.0f, 1.0f, 1.0f);
                 glVertex2f(0, 0);
@@ -30,7 +65,7 @@ void draw(void)
 
         glPointSize(10.0); // Define dot size
 
-        glFlush();
+        glutSwapBuffers();
 }
 
 void on_mouseClick(int button, int click_state, int x_mouse_position, int y_mouse_position)
@@ -49,19 +84,17 @@ void on_mouseClick(int button, int click_state, int x_mouse_position, int y_mous
 int main(int argc, char* argv[])
 {
         glutInit(&argc, argv); // Instanciate Glut
-        glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); // Defines the buffer display mode
+        glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB); // Defines the buffer display mode
         glutInitWindowSize(800, 600); // Defines the size in pixels of the window
         glutCreateWindow("Windmill"); // Defines the window title
 
-        // TODO: reshape function
-
         glutDisplayFunc(draw); // Set rendering function as "draw()"
-
-	glutIdleFunc(draw); // Set draw to repeat while no events occur
-
+        glutReshapeFunc(reshape); // Set reshaping function as "reshape()"
+        glutIdleFunc(draw); // Set draw to repeat while no events occur
         glutMouseFunc(on_mouseClick); // Handles mouse clicks
 
-        gluOrtho2D(-400, 400, -300, 300); // Defines the orthogonal plane to build the scene in
+        glMatrixMode(GL_PROJECTION);
+        gluOrtho2D(-ORTHO_X, ORTHO_X, -ORTHO_Y, ORTHO_Y); // Defines the orthogonal plane to build the scene in
 
         glutMainLoop(); // Start operations according to the specifications above
 }
